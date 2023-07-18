34f8d71acc02cb852b32d4bd97b397f7e7b9a6bf
jose angel <joseangelcuervo802@gmail.com>
2023-07-18 15:02:22 +0200

    creacion biblioteca

---
+++ java/bootcamp/src/main/java/com/nttdata/bootcamp/biblioteca/mapper/BiblioMapper.java
@@ -0,0 +1,11 @@
+package com.nttdata.bootcamp.biblioteca.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.nttdata.bootcamp.biblioteca.model.Libro;
+import com.nttdata.core.crud.mapper.CrudMapper;
+
+@Mapper
+public interface BiblioMapper extends CrudMapper<Libro>{
+
+}
---
+++ java/bootcamp/src/main/java/com/nttdata/bootcamp/biblioteca/service/BiblioService.java
@@ -0,0 +1,8 @@
+package com.nttdata.bootcamp.biblioteca.service;
+
+import com.nttdata.bootcamp.biblioteca.model.Libro;
+import com.nttdata.core.crud.service.CrudService;
+
+public interface BiblioService  extends CrudService<Libro>{
+
+}
