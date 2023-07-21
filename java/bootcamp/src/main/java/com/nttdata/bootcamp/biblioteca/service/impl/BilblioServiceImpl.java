6f9eabee19fcaa472be491b520b35f09ce177bca
jose angel <joseangelcuervo802@gmail.com>
2023-07-21 13:59:05 +0200

    cambios dia 21/07/2023

--- java/bootcamp/src/main/java/com/nttdata/bootcamp/biblioteca/service/impl/BilblioServiceImpl.java
+++ java/bootcamp/src/main/java/com/nttdata/bootcamp/biblioteca/service/impl/BilblioServiceImpl.java
@@ -1,12 +1,14 @@
 package com.nttdata.bootcamp.biblioteca.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.nttdata.bootcamp.biblioteca.mapper.BiblioMapper;
 import com.nttdata.bootcamp.biblioteca.model.Libro;
 import com.nttdata.bootcamp.biblioteca.service.BiblioService;
 import com.nttdata.core.crud.mapper.CrudMapper;
 
+@Service
 public class BilblioServiceImpl implements BiblioService{
 
 	@Override
