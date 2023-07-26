4ea368798887e4254d01197e561b00213627e719
jose angel <joseangelcuervo802@gmail.com>
2023-07-26 08:53:47 +0200

    completo

---
+++ java/bootcamp/src/main/java/com/nttdata/bootcamp/demo/model/Demo.java
@@ -0,0 +1,17 @@
+package com.nttdata.bootcamp.demo.model;
+
+import com.nttdata.core.common.model.Core;
+
+import lombok.Data;
+
+
+@Data
+public class Demo extends Core<String>{
+
+
+private static final long serialVersionUID = 1L;
+	
+	private String atributo1;
+
+}
+
